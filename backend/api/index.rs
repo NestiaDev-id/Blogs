3dff8a981f66ced8af5de390e1e2c991504ad251
NestiaDev-id <yohanesdevano90@gmail.com>
2026-07-04 15:51:28 +0700

    feat: update backend

--- backend/api/index.rs
+++ backend/api/index.rs
@@ -6,26 +6,19 @@ async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     let response_body = json!({
         "message": "Welcome to Lentera Novel API!",
         "status": "online"
-    }).to_string();
+    })
+    .to_string();
 
-    let accept_header = req.headers()
-        .get("accept")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("");
-
-    let mut response = Response::builder().status(StatusCode::OK);
-
-    if accept_header.contains("text/html") {
-        response = response.header("Content-Type", "text/plain");
-    } else {
-        response = response.header("Content-Type", "application/json");
-    }
-
-    Ok(response
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "text/plain")
         .header("Access-Control-Allow-Origin", "*")
-        .body(Body::from(response_body))?)
+        .body(Body::from(response_body))
+        .expect("Internal Server Error");
+
+    Ok(response)
 }
