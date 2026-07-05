ade97e4568705698fc91b3b03d45788bc1f50f91
NestiaDev-id <yohanesdevano90@gmail.com>
2026-07-05 23:57:39 +0700

    feat: implementation simple backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => c.redirect('/api'))
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+// Root API
+app.get('/api', (c) => c.json({ 
+  message: "Welcome to Lentera Blog API",
+  version: "1.0.0"
+}))
+
+// Error Handler
+app.onError((err, c) => c.json({ status: "error", message: err.message }, 500));
+
+// error 404 handler
+app.notFound((c) => c.json({ status: "error", message: "Not Found" }, 404));
+
+const port = 3000;
+console.log(`Server running on http://localhost:${port}`);
+
+serve({ fetch: app.fetch, port });
